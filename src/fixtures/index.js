b40a420fd2ac3f6e9b3e5fd5fcb3755580390a04
Gameonn <jindal.ankit89@gmail.com>
2022-09-01 18:22:06 +0530

    Add fixtures for tests

---
+++ src/fixtures/index.js
@@ -0,0 +1,34 @@
+export const fakeMovieDetail = {
+  Title: "Recon 2022: The Mezzo Incident",
+  Year: "2007",
+  Rated: "Not Rated",
+  Released: "11 Mar 2008",
+  Runtime: "90 min",
+  Genre: "Sci-Fi",
+  Director: "Christian Viel",
+  Writer: "William Alexander, John Fallon, Christian Viel",
+  Actors: "Andy Bradshaw, Heidi Hawkins, Deke Richards",
+  Plot: "After the gruesome events on Caprini, Sharp (Anderson Bradshaw) and his elite team of tough, witty and trigger happy space Marines must now head out to the icy planet Mezzo to investigate further Ma'har activities. What they will ...",
+  Language: "English",
+  Country: "Canada",
+  Awards: "N/A",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMTY5MDE1NjAwMF5BMl5BanBnXkFtZTcwODE1MDU3MQ@@._V1_SX300.jpg",
+  Ratings: [{ Source: "Internet Movie Database", Value: "2.3/10" }],
+  Metascore: "N/A",
+  imdbRating: "2.3",
+  imdbVotes: "529",
+  imdbID: "tt0791142",
+  Type: "movie",
+  DVD: "08 Jan 2017",
+  BoxOffice: "N/A",
+  Production: "N/A",
+  Website: "N/A",
+  Response: "True",
+};
+
+export const fakeUserInput = "hello";
+
+export const fakeSelectedMovie = "tt0791142";
+
+export const invalidMovieId = "tt2878342342";
