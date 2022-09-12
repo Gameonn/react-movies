fb7e73a08562ab545e1e16c3cf078e29048cbefb
Gameonn <jindal.ankit89@gmail.com>
2022-09-12 16:55:17 +0530

    Add basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Header />
       <Routes>
         {routes.map((route) => (
